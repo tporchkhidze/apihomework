4528182e1f27d26590a1fc4db4bd5ac645891f60
Tepo <56931397+Tepo@users.noreply.github.com>
2021-01-03 22:42:28 +0400

    message

---
+++ homeworkapi/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return User::all();
+    }
+
+    public function show(User $user){
+        return $user;
+    }
+}
---
+++ homeworkapi/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/login', [\App\Http\Controllers\Api\Auth\AuthController::class, 'login'])->name('api.users.login');
+
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index'])->name('api.users.index');
+    Route::get('/users/{user}', [\App\Http\Controllers\Api\UserController::class, 'index'])->name('api.users.show');
+});
